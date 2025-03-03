1298694a6b15a701163cb36382cfa77ec78a6479
Rkr <sukant547@gmail.com>
2025-03-03 15:30:16 +0545

    Revert back to older version of the original utils'

--- app/src/main/java/com/SukantRajkarnikar/cloud_storage_util_v3/AWSUtils.kt
+++ app/src/main/java/com/SukantRajkarnikar/cloud_storage_util_v3/AWSUtils.kt
@@ -13,56 +13,21 @@ import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ListObjectsV2Request
 import com.amazonaws.services.s3.model.ListObjectsV2Result
-import com.sukantrajkarnikar.cloud_storage_util_v3.errors.ImageCorruptedException
 import java.io.*
-import java.lang.ref.WeakReference
 
-class AWSUtils private constructor(context: Context?) {
-    private var weakContext: WeakReference<Context>? = null
 
-    init {
-        this.weakContext = WeakReference(context)
-    }
-
+class AWSUtils(
+    private val context: Context,
+    val onAwsImageUploadListener: OnAwsImageUploadListener,
+) {
     private var observer: TransferObserver? = null
     private var imageFile: File? = null
     private var mTransferUtility: TransferUtility? = null
     private var sS3Client: AmazonS3Client? = null
     private var sCredProvider: CognitoCachingCredentialsProvider? = null
     private lateinit var awsMetaInfo: AwsMetaInfo
+    private val MAX_RETRY_COUNT: Int = 2
     private var retryCount = 0
-    private var onAwsImageUploadListener: OnAwsImageUploadListener? = null
-
-    companion object {
-        private const val MAX_RETRY_COUNT: Int = 2
-        private var awsUtils: AWSUtils? = null
-
-        @Synchronized
-        internal fun getInstance(context: Context?): AWSUtils? {
-            if (null == awsUtils) {
-                awsUtils = AWSUtils(context)
-            }
-            return awsUtils
-        }
-
-        fun get(): AWSUtils? {
-            return awsUtils
-        }
-    }
-
-    private fun getContext(): Context? {
-        return this.weakContext?.get()
-    }
-
-    @Deprecated("replaced by kotlin-lamda function.")
-    fun setListener(onAwsImageUploadListener: OnAwsImageUploadListener) {
-        this.onAwsImageUploadListener = onAwsImageUploadListener
-    }
-
-    @Deprecated("replaced by kotlin-lamda function.")
-    fun removeListener() {
-        this.onAwsImageUploadListener = null
-    }
 
     private fun getCredProvider(context: Context): CognitoCachingCredentialsProvider? {
         if (sCredProvider == null) {
@@ -85,11 +50,11 @@ class AWSUtils private constructor(context: Context?) {
         }
 
         if (sS3Client == null) {
-            sS3Client = AmazonS3Client(
-                getCredProvider(context!!),
-                Region.getRegion(awsMetaInfo.serviceConfig.region),
-                clientConfiguration
-            )
+            sS3Client =
+                AmazonS3Client(
+                    getCredProvider(context!!),
+                    Region.getRegion(awsMetaInfo.serviceConfig.region), clientConfiguration
+                )
         }
         return sS3Client
     }
@@ -101,89 +66,73 @@ class AWSUtils private constructor(context: Context?) {
             // 10 threads for upload and download operations.
 
             // Initializes TransferUtility
-            mTransferUtility =
-                TransferUtility.builder().s3Client(getS3Client(context.applicationContext))
-                    .context(context.applicationContext).transferUtilityOptions(tuOptions).build()
+            mTransferUtility = TransferUtility
+                .builder()
+                .s3Client(getS3Client(context.applicationContext))
+                .context(context.applicationContext)
+                .transferUtilityOptions(tuOptions)
+                .build()
         }
         return mTransferUtility
     }
 
-    fun beginUpload(
-        awsMetaInfo: AwsMetaInfo,
-        showProgress: () -> Unit,
-        onSuccess: (String, AwsMetaInfo) -> Unit,
-        onError: (Throwable, AwsMetaInfo) -> Unit,
-        onStateChanged: (String) -> Unit,
-        onProgressChanged: (Int, byteCurrent: Float, byteTotal: Float) -> Unit,
-    ) {
+    fun beginUpload(awsMetaInfo: AwsMetaInfo, onSuccess: (String) -> Unit) {
         this.awsMetaInfo = awsMetaInfo
         if (TextUtils.isEmpty(awsMetaInfo.imageMetaInfo.imagePath)) {
-            onError(
-                FileNotFoundException("Could not find the filepath of the selected file"),
-                awsMetaInfo
-            )
-            /* onAwsImageUploadListener?.onError(
-                 FileNotFoundException("Could not find the filepath of the selected file"),
-                 awsMetaInfo
-             )*/
+            onAwsImageUploadListener.onError("Could not find the filepath of the selected file")
             return
         }
 
         val oldExif = ExifInterface(awsMetaInfo.imageMetaInfo.imagePath)
-        val compressedImage = compressAwsImage(awsMetaInfo)
-        val compressedImagePath = compressedImage.first
-        val compressedBitmap = compressedImage.second
+        val compressedImagePath = compressAwsImage(awsMetaInfo).first
+        val compressedBitmap = compressAwsImage(awsMetaInfo).second
         val newExifOrientation = setImageOrientation(oldExif, compressedImagePath)
-        if (newExifOrientation == null) {
-            onError(
-                ImageCorruptedException("Cannot change orientation of image. Image may be corrupted."),
-                awsMetaInfo
-            )
-            /* onAwsImageUploadListener?.onError(
-                 ImageCorruptedException("Cannot change orientation of image. Image may be corrupted."),
-                 awsMetaInfo
-             )*/
-            return
-        }
-        try {
-            val rotation = getRotation(newExifOrientation)
-            if (rotation != null) {
-                val matrix = Matrix()
-                matrix.postRotate(rotation)
-                setPostScale(newExifOrientation, matrix)
-                if (compressedBitmap != null) {
-                    val rotatedBitmap = Bitmap.createBitmap(
-                        compressedBitmap,
-                        0,
-                        0,
-                        compressedBitmap.width,
-                        compressedBitmap.height,
-                        matrix,
-                        true
-                    )
-                    if (rotatedBitmap != null) {
-                        // rotatedBitmap will be recycled inside addAwsWaterMark function
-                        val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, rotatedBitmap)
-                        waterMarkBitmap.recycle()
+
+        if (newExifOrientation != null) {
+            try {
+                val rotation = getRotation(newExifOrientation)
+                if (rotation != null) {
+                    val matrix = Matrix()
+                    matrix.postRotate(rotation)
+                    setPostScale(newExifOrientation, matrix)
+                    if (compressedBitmap != null) {
+                        val rotatedBitmap = Bitmap.createBitmap(
+                            compressedBitmap,
+                            0,
+                            0,
+                            compressedBitmap.width,
+                            compressedBitmap.height,
+                            matrix,
+                            true
+                        )
+                        if (rotatedBitmap != null) {
+                            // rotatedBitmap will be recycled inside addAwsWaterMark function
+                            val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, rotatedBitmap)
+                            waterMarkBitmap.recycle()
+                        }
+                        compressedBitmap.recycle()
                     }
-                    compressedBitmap.recycle()
-                }
-            } else {
-                if (compressedBitmap != null) {
-                    val newBitmap = Bitmap.createBitmap(
-                        compressedBitmap, 0, 0, compressedBitmap.width, compressedBitmap.height
-                    )
-                    if (newBitmap != null) {
-                        // newBitmap will be recycled inside addAwsWaterMark function
-                        val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, newBitmap)
-                        waterMarkBitmap.recycle()
+                } else {
+                    if (compressedBitmap != null) {
+                        val newBitmap = Bitmap.createBitmap(
+                            compressedBitmap,
+                            0,
+                            0,
+                            compressedBitmap.width,
+                            compressedBitmap.height
+                        )
+                        if (newBitmap != null) {
+                            // newBitmap will be recycled inside addAwsWaterMark function
+                            val waterMarkBitmap = addAwsWaterMark(awsMetaInfo, newBitmap)
+                            waterMarkBitmap.recycle()
+                        }
+                        compressedBitmap.recycle()
                     }
-                    compressedBitmap.recycle()
                 }
+            } catch (error: Exception) {
+                error.printStackTrace()
+                awsMetaInfo.imageMetaInfo.imagePath = compressedImagePath
             }
-        } catch (error: Exception) {
-            error.printStackTrace()
-            awsMetaInfo.imageMetaInfo.imagePath = compressedImagePath
         }
         /*  if (compressedBitmap != null) {
               val newBitmap = Bitmap.createBitmap(
@@ -203,59 +152,43 @@ class AWSUtils private constructor(context: Context?) {
 
         val file = File(awsMetaInfo.imageMetaInfo.imagePath)
         imageFile = file
-//        onAwsImageUploadListener?.showProgress()
-        showProgress()
+        onAwsImageUploadListener.showProgress()
 
-        observer = getContext()?.let {
-            getTransferUtility(it)?.upload(
-                awsMetaInfo.serviceConfig.bucketName, //Bucket name
-                "${awsMetaInfo.awsFolderPath}/${imageFile?.name}", imageFile
-            )
-        }
-        observer?.setTransferListener(
-            UploadListener(
-                onSuccess, onError, onStateChanged, onProgressChanged
-            )
+        observer = getTransferUtility(context)?.upload(
+            awsMetaInfo.serviceConfig.bucketName, //Bucket name
+            "${awsMetaInfo.awsFolderPath}/${imageFile?.name}", imageFile
         )
+        observer?.setTransferListener(UploadListener(onSuccess))
     }
 
-    private inner class UploadListener(
-        private val onSuccess: (String, AwsMetaInfo) -> Unit,
-        private val onError: (Throwable, AwsMetaInfo) -> Unit,
-        private val onStateChanged: (String) -> Unit,
-        private val onProgressChanged: (Int, byteCurrent: Float, byteTotal: Float) -> Unit,
-    ) : TransferListener {
+    private inner class UploadListener(private val onSuccess: (String) -> Unit) : TransferListener {
         override fun onError(id: Int, e: Exception) {
-//            onAwsImageUploadListener?.onError(e, awsMetaInfo)
-            onError(e, awsMetaInfo)
+            onAwsImageUploadListener.onError(e.message.toString())
         }
 
         override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
-            onProgressChanged(id, bytesCurrent.toFloat(), bytesTotal.toFloat())
-            /* onAwsImageUploadListener?.onProgressChanged(
-                 id,
-                 bytesCurrent.toFloat(),
-                 bytesTotal.toFloat()
-             )*/
+            onAwsImageUploadListener.onProgressChanged(
+                id,
+                bytesCurrent.toFloat(),
+                bytesTotal.toFloat()
+            )
         }
 
         override fun onStateChanged(id: Int, newState: TransferState) {
             if (newState == TransferState.COMPLETED) {
                 val finalImageUrl =
                     "${awsMetaInfo.serviceConfig.url}${awsMetaInfo.awsFolderPath}/${imageFile?.name}"
-//                onAwsImageUploadListener?.onSuccess(finalImageUrl)
-                onSuccess(finalImageUrl, awsMetaInfo)
+                onAwsImageUploadListener.onSuccess(finalImageUrl)
+                onSuccess(finalImageUrl)
             } else if (newState == TransferState.CANCELED) {
-//                onAwsImageUploadListener?.onStateChanged(getState(newState))
-                onStateChanged(getState(newState))
+                onAwsImageUploadListener.onStateChanged(getState(newState))
             } else if (newState == TransferState.FAILED) {
                 if (retryCount != MAX_RETRY_COUNT) {
                     retryCount += 1
                     observer = mTransferUtility?.resume(id)
                     observer?.setTransferListener(this)
                 } else {
-//                    onAwsImageUploadListener?.onStateChanged(getState(newState))
-                    onStateChanged(getState(newState))
+                    onAwsImageUploadListener.onStateChanged(getState(newState))
                 }
             }
         }
@@ -265,10 +198,9 @@ class AWSUtils private constructor(context: Context?) {
         return try {
             val byteArray = streamToByteArray(FileInputStream(awsMetaInfo.imageMetaInfo.imagePath))
             val bitmap = decodeSampledBitmapFromResource(
-                byteArray,
-                awsMetaInfo.imageMetaInfo.imageWidth ?: AwsConstant.DEFAULT_IMAGE_WIDTH,
-                awsMetaInfo.imageMetaInfo.imageHeight ?: AwsConstant.DEFAULT_IMAGE_HEIGHT,
-                awsMetaInfo.imageMetaInfo.waterMarkInfo
+                byteArray, awsMetaInfo.imageMetaInfo.imageWidth
+                    ?: AwsConstant.DEFAULT_IMAGE_WIDTH, awsMetaInfo.imageMetaInfo.imageHeight
+                    ?: AwsConstant.DEFAULT_IMAGE_HEIGHT, awsMetaInfo.imageMetaInfo.waterMarkInfo
             )
 
             val stream = ByteArrayOutputStream()
@@ -332,20 +264,15 @@ class AWSUtils private constructor(context: Context?) {
     }
 
     private fun setImageOrientation(oldExif: ExifInterface, newImagePath: String): Int? {
-        try {
-            val exifOrientation = oldExif.getAttribute(ExifInterface.TAG_ORIENTATION)
-            if (exifOrientation != null) {
-                val newExif = ExifInterface(newImagePath)
-                newExif.setAttribute(ExifInterface.TAG_ORIENTATION, exifOrientation)
-                newExif.saveAttributes()
-                return newExif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED
-                )
-            }
-
-        } catch (error: Exception) {
-            error.printStackTrace()
-            return null
+        val exifOrientation = oldExif.getAttribute(ExifInterface.TAG_ORIENTATION)
+        if (exifOrientation != null) {
+            val newExif = ExifInterface(newImagePath)
+            newExif.setAttribute(ExifInterface.TAG_ORIENTATION, exifOrientation)
+            newExif.saveAttributes()
+            return newExif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
         }
         return null
     }
@@ -368,21 +295,24 @@ class AWSUtils private constructor(context: Context?) {
 
         if (sCredProvider == null) {
             sCredProvider = CognitoCachingCredentialsProvider(
-                getContext()?.applicationContext, poolId, getRegion(region)
+                context.applicationContext,
+                poolId,
+                getRegion(region)
             )
         }
 
         if (sS3Client == null) {
-            sS3Client = AmazonS3Client(
-                getContext()?.let { getCredProvider(it) },
-                Region.getRegion(region),
-                clientConfiguration
-            )
+            sS3Client =
+                AmazonS3Client(
+                    getCredProvider(context),
+                    Region.getRegion(region), clientConfiguration
+                )
         }
 
         Thread {
             try {
-                val listing: ListObjectsV2Result = sS3Client!!.listObjectsV2(req)
+                val listing: ListObjectsV2Result =
+                    sS3Client!!.listObjectsV2(req)
                 for (commonPrefix in listing.commonPrefixes) {
                     println("Common prefix--->$commonPrefix")
                 }
@@ -411,12 +341,11 @@ class AWSUtils private constructor(context: Context?) {
         }.start()
     }
 
-    @Deprecated("This listener won't work from version (2.2.2), replaced by kotlin-lamda function.")
     interface OnAwsImageUploadListener {
         fun showProgress()
         fun onProgressChanged(id: Int, currentByte: Float, totalByte: Float)
         fun onSuccess(imgUrl: String)
-        fun onError(error: Throwable, awsMetaInfo: AwsMetaInfo)
+        fun onError(errorMsg: String)
         fun onStateChanged(state: String)
     }
 }
--- app/src/main/java/com/SukantRajkarnikar/cloud_storage_util_v3/errors/CloudStoreUtilException.kt
+++
@@ -1,5 +0,0 @@
-package com.sukantrajkarnikar.cloud_storage_util_v3.errors
-
-import java.io.IOException
-
-class ImageCorruptedException(var errorMsg: String): IOException()
